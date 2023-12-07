3840c49e78491c921dd0754eafc9bbd2ea324aed
Muliro <mulirokhaemba@gmail.com>
2023-12-07 03:25:40 +0300

    clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
+import os
+from fabric.api import *
+
+env.user = 'ubuntu'
+env.hosts = ['100.25.165.125', '100.25.165.125']
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [x for x in archives if "web_static_" in x]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(x)) for x in archives]
